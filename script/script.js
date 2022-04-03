85982f0050b33b78f29e66f6908312957e65aee6
pb-bennett <pb.bennett.uk@gmail.com>
2022-04-03 22:51:30 +0200

    grid created.  needs refining

--- script/script.js
+++ script/script.js
@@ -1,2 +1,23 @@
-"use strict";
-console.log("Hello World");
+'use strict';
+
+const gridContainer = document.getElementById('grid-container');
+
+const cell = document.createElement('div');
+
+// cell.innerHTML = 'Text';
+// gridContainer.appendChild(cell);
+
+for (let i = 1; i < 17; i++) {
+  for (let j = 1; j < 17; j++) {
+    const cell = document.createElement('div');
+    cell.innerHTML = i.toString() + ' ' + j.toString();
+    cell.className = 'cell';
+    cell.dataset.cell = `${i}--${j}`;
+    cell.style.border = '2px black solid';
+    gridContainer.appendChild(cell);
+  }
+}
+
+// let i = 2;
+// let j = 3;
+// console.log(i.toString() + j.toString());
